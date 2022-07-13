3344d3dee9d4f1cc3e773b354cea47560b5cf877
Ilyas SULI22 <ilyas@clswl069.cels.anl.gov>
2022-07-13 12:42:29 -0500

    changed text_message

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -80,7 +80,7 @@ class sourcetrackerV2:
         
         report = KBaseReport(self.callback_url)
         report_info = report.create({'report': {'objects_created':[],
-                                                'text_message': 'Proportion Tables %a %b %c' % mpm, mps, fas},
+                                                'text_message': 'Proportion Tables'},
                                                 'workspace_name': params['workspace_name']})
         output = {
             'report_name': report_info['name'],
