8d12a642863e416a78651060d736fc623be8ac09
Ilyas SULI22 <ilyas@clswl072.cels.anl.gov>
2022-07-15 10:55:37 -0500

    added _sourcetrackerUtil import back and removed skbio

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import copy
 from functools import partial
-from skbio.stats import subsample_counts
+import sourcetrackerV2.Util._sourcetrackerUtil import _sourcetrackerUtil
 from installed_clients.KBaseReportClient import KBaseReport
 #END_HEADER
 
