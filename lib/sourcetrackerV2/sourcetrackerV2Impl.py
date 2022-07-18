d80e68a596b0c31a2b89a462aa5c4f3aec40876d
Ilyas SULI22 <ilyas@clswl064.cels.anl.gov>
2022-07-18 13:54:59 -0500

    made html links a dict

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -1079,7 +1079,7 @@ class sourcetrackerV2:
         report_params = {
         'message': message,
         'workspace_name': params['workspace_name'],
-        'html_links': [mpm_html],
+        'html_links': ['Proportion Table': mpm_html],
             'direct_html_link_index': 0,
             'direct_html': mpm_html,
             'html_window_height': 333,
