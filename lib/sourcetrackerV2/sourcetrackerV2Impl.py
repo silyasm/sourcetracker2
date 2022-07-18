b5e89b9e273318360ec041eaa144445c4eee5ea7
Ilyas SULI22 <ilyas@clswl064.cels.anl.gov>
2022-07-18 09:16:05 -0500

    changed message

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -1045,7 +1045,7 @@ class sourcetrackerV2:
         sources = []
         neither = []
         row_ids = ''
-        message = str(mpm.iat[0,0]) + '' + str(amp_matrix.iat[0,0])
+        message = str(mpm.iat[0,0])
         #for i in amp_data:
             #row_ids += i
         #col_ids = amp_data['data']['col_ids']
