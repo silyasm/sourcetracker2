0ee58d592356bcdf0b1b4d97afe3c1c5d8621b93
Ilyas SULI22 <ilyas@clswl064.cels.anl.gov>
2022-07-18 13:59:02 -0500

    tried to make html links a dict

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -1079,7 +1079,7 @@ class sourcetrackerV2:
         report_params = {
         'message': message,
         'workspace_name': params['workspace_name'],
-        'html_links': ['Proportion Table': mpm_html],
+        'html_links': dict[],
             'direct_html_link_index': 0,
             'direct_html': mpm_html,
             'html_window_height': 333,
