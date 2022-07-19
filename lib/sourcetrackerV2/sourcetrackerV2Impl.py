1936ee344c39b349c62263856783bd780e6fab65
silyasm <78037911+silyasm@users.noreply.github.com>
2022-07-18 22:01:59 -0500

    removed mkdir

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -1014,19 +1014,7 @@ class sourcetrackerV2:
             df = pd.DataFrame(index=row_ids, columns=col_ids)
         
             return df
-        def _mkdir_p(self, path):
-            """
-            _mkdir_p: make directory for given path
-            """
-            if not path:
-                return
-            try:
-                os.makedirs(path)
-            except OSError as exc:
-                if exc.errno == errno.EEXIST and os.path.isdir(path):
-                    pass
-                else:
-                    raise
+
         def _build_table_content(self, output_directory, matrix_df):
             """
             _build_table_content: generate HTML table content for FloatMatrix2D object
@@ -1112,7 +1100,6 @@ class sourcetrackerV2:
             html_report = list()
 
             output_directory = os.path.join(self.scratch, str(uuid.uuid4()))
-            self._mkdir_p(output_directory)
             result_file_path = os.path.join(output_directory, 'matrix_report.html')
 
             visualization_content = self._generate_visualization_content(output_directory, matrix_df)
