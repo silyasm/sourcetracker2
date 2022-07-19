66ab44c180b1812db5f70aff8692a489dcd5621e
Ilyas SULI22 <ilyas@clswl083.cels.anl.gov>
2022-07-19 10:26:49 -0500

    added message with mpm output

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -1222,7 +1222,7 @@ class sourcetrackerV2:
         sources = []
         neither = []
         row_ids = ''
-        message = 'hello'
+        message = str(mpm.iat(0,3))
         
         mpm_html = mpm.to_html()
         
@@ -1259,19 +1259,14 @@ class sourcetrackerV2:
         
         #report_shock_id = self.dfu.file_to_shock({'file_path': output_directory,'pack': 'zip'})['shock_id']
         
-        report_params = {
-            'message': message,
-            'workspace_name': params['workspace_name'],
-            #'html_links': html_report,
-            #'direct_html_link_index': 0,
-            #'html_window_height': 333,
+        report = KBaseReport(self.callback_url)
+        report_info = report.create({'report': {'objects_created':[],
+                                                'text_message': message},
+                                                'workspace_name': params['workspace_name']})
+        output = {
+            'report_name': report_info['name'],
+            'report_ref': report_info['ref'],
         }
-        
-        kbase_report_client = KBaseReport(self.callback_url)
-        output = kbase_report_client.create_extended_report(report_params)
-
-        report_output = {'report_name': output['name'],
-                          'report_ref': output['ref']}
                
         #END run_sourcetrackerV2
 
