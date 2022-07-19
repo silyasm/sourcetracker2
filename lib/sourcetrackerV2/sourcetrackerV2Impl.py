1ddef62e0cfd3d57ed08d2ac0246d262f9bd4632
silyasm <78037911+silyasm@users.noreply.github.com>
2022-07-18 21:28:55 -0500

    added self.scratch

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -46,6 +46,7 @@ class sourcetrackerV2:
         #BEGIN_CONSTRUCTOR
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         self.shared_folder = config['scratch']
+        self.scratch = config['scratch']
         logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
                             level=logging.INFO)
         #END_CONSTRUCTOR
