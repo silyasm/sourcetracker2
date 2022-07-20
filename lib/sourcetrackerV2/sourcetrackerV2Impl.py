e942b4386fc30d3b9edb12288257d08650848511
silyasm <78037911+silyasm@users.noreply.github.com>
2022-07-19 20:06:37 -0500

    added message and html links to report output

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -1203,7 +1203,9 @@ class sourcetrackerV2:
         output = kbase_report_client.create_extended_report(report_params)
 
         report_output = {'report_name': output['name'],
-                          'report_ref': output['ref']}
+                        'report_ref': output['ref'],
+                        'report_message': output['message'].
+                        'report_html_links': output['html_links']}
         returnVal = {}
         returnVal.update(report_output)
         #END run_sourcetrackerV2
