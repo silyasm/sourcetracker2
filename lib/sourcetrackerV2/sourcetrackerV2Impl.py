c2716f8f8c96d3c408d80a16f11cd81501357aee
Ilyas SULI22 <ilyas@clswl073.cels.anl.gov>
2022-07-22 11:05:02 -0500

    added data to samp_df

--- lib/sourcetrackerV2/sourcetrackerV2Impl.py
+++ lib/sourcetrackerV2/sourcetrackerV2Impl.py
@@ -1233,6 +1233,7 @@ class sourcetrackerV2:
             type_data.append(attr_obj['data'][0]['data']['instances'][sample][type_index])
 
         samp_df = pd.DataFrame(index=mdf_indx, columns=[sample_type])
+        sampl_df[sample_type] = type_data
         
         #Seperate Sink and Source samples into distinct dataframes
         sink_list = []
